53b30bae92d468109f1f5ca9f560600bdcea0128
SeacenLiu <1016667972@qq.com>
2019-05-06 21:36:48 +0800

    Cpp Mp3Encoder 功能

--- Mp3Encoder/Mp3Encoder/mp3_encoder.h
+++ Mp3Encoder/Mp3Encoder/mp3_encoder.h
@@ -21,6 +21,16 @@ private:
 public:
     Mp3Encoder();
     ~Mp3Encoder();
+    /**
+     初始化Mp3编码器
+
+     @param pcmFilePath pcm文件路径
+     @param mp3FilePath mp3文件路径
+     @param sampleRate 采样率
+     @param channels 声道数
+     @param bitRate 比特率
+     @return 0表示成功 -1表示失败
+     */
     int Init(const char* pcmFilePath, const char* mp3FilePath, int sampleRate, int channels, int bitRate);
     void Encode();
     void Destory();
