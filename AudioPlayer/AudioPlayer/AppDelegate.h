cb2a12e113aad07fe70de685261976f80bb97b4a
SeacenLiu <1016667972@qq.com>
2019-11-14 11:36:57 +0800

    init AudioPlayer Demo

---
+++ AudioPlayer/AudioPlayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AudioPlayer
+//
+//  Created by SeacenLiu on 2019/11/14.
+//  Copyright © 2019 SeacenLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AudioPlayer/AudioPlayer/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AudioPlayer
+//
+//  Created by SeacenLiu on 2019/11/14.
+//  Copyright © 2019 SeacenLiu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
