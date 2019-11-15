5b1652c008f4b86fd0f46026cc0771d7da0aa20f
SeacenLiu <1016667972@qq.com>
2019-11-15 17:51:01 +0800

    文件结构、第三方依赖、工具准备

---
+++ OpenGLRenderer/OpenGLRenderer/utils/CommonUtil.m
@@ -0,0 +1,23 @@
+//
+//  CommonUtil.m
+//  OpenGLRenderer
+//
+//  Created by SeacenLiu on 2019/11/15.
+//  Copyright © 2019 SeacenLiu. All rights reserved.
+//
+
+#import "CommonUtil.h"
+
+@implementation CommonUtil
+
++ (NSString *)bundlePath:(NSString *)fileName {
+    return [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:fileName];
+}
+
++ (NSString *)documentsPath:(NSString *)fileName {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    return [documentsDirectory stringByAppendingPathComponent:fileName];
+}
+
+@end
