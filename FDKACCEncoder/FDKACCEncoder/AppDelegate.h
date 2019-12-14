89b5aed34599a4476b00d26745544185722b9db5
SeacenLiu <1016667972@qq.com>
2019-12-14 15:20:37 +0800

    FDKACCEncoder 准备工作

---
+++ FDKACCEncoder/FDKACCEncoder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FDKACCEncoder
+//
+//  Created by SeacenLiu on 2019/12/14.
+//  Copyright © 2019 SeacenLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
