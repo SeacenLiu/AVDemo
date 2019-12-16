4437aaa92f4d4b3bf55518e50f76bed6b79512f4
SeacenLiu <1016667972@qq.com>
2019-12-16 19:28:33 +0800

    AudioToolboxEncoder init

---
+++ AudioToolboxEncoder/AudioToolboxEncoder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AudioToolboxEncoder
+//
+//  Created by SeacenLiu on 2019/12/16.
+//  Copyright © 2019 SeacenLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
